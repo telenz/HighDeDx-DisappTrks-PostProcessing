6315ffb88af2682d7579d8c1eba87d9602c409fc
Teresa Lenz <teresa.lenz@desy.de>
2015-07-31 13:37:16 +0200

    change cp to mv

--- systematics/signal/10_TrkRecoEff/runSystematicUncertainty.sh
+++ systematics/signal/10_TrkRecoEff/runSystematicUncertainty.sh
@@ -6,13 +6,13 @@ ptCut=50
 # edit analyzer.cc file and run analysis
 cd removeHits
 sed -e "s/MuonTracksAllHits_pt\[i\] < __ptCut__/MuonTracksAllHits_pt\[i\] < ${ptCut}/" analyzer.cc > analyzerAux.cc
-cp analyzerAux.cc analyzer.cc
+mv analyzerAux.cc analyzer.cc
 #make clean
 make
 ./analyzer filelist_data.txt analyzer_histogram_data.root > ../result_data.log
 ./analyzer filelist_mc.txt   analyzer_histogram_mc.root   > ../result_mc.log
 sed -e "s/MuonTracksAllHits_pt\[i\] < ${ptCut}/MuonTracksAllHits_pt[i] < __ptCut__/" analyzer.cc > analyzerAux.cc
-cp analyzerAux.cc analyzer.cc
+mv analyzerAux.cc analyzer.cc
 cd ..
 ###########################################################################################################################
 
