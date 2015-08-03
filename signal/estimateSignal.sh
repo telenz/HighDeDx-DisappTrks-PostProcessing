2e63a64e211bcc6752d7437c551d62cfa63da49c
Teresa Lenz <teresa.lenz@desy.de>
2015-08-03 13:00:21 +0200

    bug fix

--- signal/estimateSignal.sh
+++ signal/estimateSignal.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-#source "/afs/desy.de/user/t/tlenz/myAnalysis/bkgEstimation/variablesSetting.sh"
+#source "../variablesSetting.sh"
 
 root -l -b -q getSignalResult.C+"(${metCut},${jetptCut},${ptCut},${ecaloCut},${iasCut},\"${region}\")" > signal.log
 cat signal.log
