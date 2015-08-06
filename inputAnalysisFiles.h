0ffd7f4a458763390164877d52a114626c14d5ae
Teresa Lenz <teresa.lenz@desy.de>
2015-08-06 12:55:40 +0200

    new input files

--- inputAnalysisFiles.h
+++ inputAnalysisFiles.h
@@ -10,48 +10,40 @@ TString fileElectronCS  = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_04_MET
 TString fileMuonCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_04_METGt0_JetPt70_MuonCS/results/analyzer/ntuples/input_weighted/";
 TString fileFakeCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_04_METGt0_JetPt70_FakeCS/results/analyzer/ntuples/input_weighted/";
 
-
-/*
-TString fileSR          = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_07_21_METGt0_JetPtGt0_woBlinding/results/analyzer/ntuples/input_weighted/";
-TString filePionCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_06_04_METGt0_JetPtGt70_trackPtGt20_TauCS/results/analyzer/ntuples/input_weighted/";
-TString fileElectronCS  = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_06_04_METGt0_JetPtGt70_trackPtGt20_ElectronCS/results/analyzer/ntuples/input_weighted/";
-TString fileMuonCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_06_04_METGt0_JetPtGt70_trackPtGt20_MuonCS/results/analyzer/ntuples/input_weighted/";
-TString fileFakeCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_06_15_METGt0_JetPtGt70_trackPtGt20_d0CutNotInverted_FakeCS/results/analyzer/ntuples/input_weighted/";
-*/
-
-
-/*
-TString fileSR          = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_22_METGt0_JetPtGt0_TauPtGt30_SubJetPtGt30/results/analyzer/ntuples/input_weighted/";
-TString filePionCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_22_METGt0_JetPtGt0_TauPtGt30_SubJetPtGt30_TauCS/results/analyzer/ntuples/input_weighted/";
-TString fileElectronCS  = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_22_METGt0_JetPtGt0_TauPtGt30_SubJetPtGt30_ElectronCS/results/analyzer/ntuples/input_weighted/";
-TString fileMuonCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_22_METGt0_JetPtGt0_TauPtGt30_SubJetPtGt30_MuonCS/results/analyzer/ntuples/input_weighted/";
-TString fileFakeCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_06_15_METGt0_JetPtGt70_trackPtGt20_d0CutNotInverted_FakeCS/results/analyzer/ntuples/input_weighted/";
-*/
-
-/*
-TString fileSR          = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_08_METGt0_JetPtGt0/results/analyzer/ntuples/input_weighted/";
-TString filePionCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_08_METGt0_JetPtGt0_TauCS/results/analyzer/ntuples/input_weighted/";
-TString fileElectronCS  = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_08_METGt0_JetPtGt0_ElectronCS/results/analyzer/ntuples/input_weighted/";
-TString fileMuonCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_08_METGt0_JetPtGt0_MuonCS/results/analyzer/ntuples/input_weighted/";
-*/
-/*
-TString fileSR          = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_22_METGt0_JetPtGt0_TauPtGt30_SubJetPtGt20/results/analyzer/ntuples/input_weighted/";
-TString filePionCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_22_METGt0_JetPtGt0_TauPtGt30_SubJetPtGt20_TauCS/results/analyzer/ntuples/input_weighted/";
-TString fileElectronCS  = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_22_METGt0_JetPtGt0_TauPtGt30_SubJetPtGt20_ElectronCS/results/analyzer/ntuples/input_weighted/";
-TString fileMuonCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_22_METGt0_JetPtGt0_TauPtGt30_SubJetPtGt20_MuonCS/results/analyzer/ntuples/input_weighted/";
-*/
-/*
-TString fileSR          = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_01_METGt0_JetPtGt70_withData_andSignal/results/analyzer/ntuples/input_weighted/";
-TString filePionCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_01_METGt0_JetPtGt70_TauCS/results/analyzer/ntuples/input_weighted/";
-TString fileElectronCS  = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_01_METGt0_JetPtGt70_ElectronCS/results/analyzer/ntuples/input_weighted/";
-TString fileMuonCS      = "~/xxl-af-cms/ANALYSIS/workdir/analysis_2015_05_01_METGt0_JetPtGt70_MuonCS/results/analyzer/ntuples/input_weighted/";
-*/
-
-
-//TString fileSingleMu"/afs/desy.de/user/t/tlenz/xxl-af-cms/DYTOLLANALYSIS/workdir/analysis_2015_05_19_woStatus2Particle/results/analyzer/ntuples/input_weighted/SingleMu.root";
-//TString fileSingleElectron"/afs/desy.de/user/t/tlenz/xxl-af-cms/DYTOLLANALYSIS/workdir/analysis_2015_05_19_woStatus2Particle/results/analyzer/ntuples/input_weighted/SingleElectron.root";
-
+TString fileDYToLL         = "/afs/desy.de/user/t/tlenz/xxl-af-cms/DYTOLLANALYSIS/workdir/analysis_2015_06_16_newNtuples/results/analyzer/ntuples/input_weighted/";
 TString fileSingleMu       = "/afs/desy.de/user/t/tlenz/xxl-af-cms/DYTOLLANALYSIS/workdir/analysis_2015_06_16_newNtuples/results/analyzer/ntuples/input_weighted/SingleMu.root";
 TString fileSingleElectron = "/afs/desy.de/user/t/tlenz/xxl-af-cms/DYTOLLANALYSIS/workdir/analysis_2015_06_16_newNtuples/results/analyzer/ntuples/input_weighted/SingleElectron.root";
+
+
+// For systematic uncertainties
+// 1.) Bkg
+TString inputScaleUnc         = "/afs/desy.de/user/t/tlenz/xxl-af-cms/DYTOLLANALYSIS/workdir/analysis_2015_06_25_TagAndProbe/results/analyzer/ntuples/input_weighted/";
+TString inputScaleUncPion     = "/afs/desy.de/user/t/tlenz/xxl-af-cms/DYTOLLANALYSIS/workdir/analysis_2015_06_25_TagAndProbe_TauCS/results/analyzer/ntuples/input_weighted/";
+TString inputScaleUncMuon     = "/afs/desy.de/user/t/tlenz/xxl-af-cms/DYTOLLANALYSIS/workdir/analysis_2015_06_25_TagAndProbe_MuonCS/results/analyzer/ntuples/input_weighted/";
+TString inputScaleUncElectron = "/afs/desy.de/user/t/tlenz/xxl-af-cms/DYTOLLANALYSIS/workdir/analysis_2015_06_25_TagAndProbe_ElectronCS/results/analyzer/ntuples/input_weighted/";
+
+//2.) Signal
+TString inputISRcentral       = fileSR;
+TString inputISRup            = "/afs/desy.de/user/t/tlenz/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_06_ISRUnc_up/results/analyzer/ntuples/input_weighted/";
+TString inputISRdown          = "/afs/desy.de/user/t/tlenz/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_06_ISRUnc_down/results/analyzer/ntuples/input_weighted/";
+
+TString inputTrigEffNominal  = fileSR;
+TString inputTrigEffWeighted = "/afs/desy.de/user/t/tlenz/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_06_TrigEffunc_weighted/results/analyzer/ntuples/input_weighted/";
+
+TString inputJEScentral      = fileSR;
+TString inputJESup           = "/afs/desy.de/user/t/tlenz/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_06_JESunc_up/results/analyzer/ntuples/input_weighted/";
+TString inputJESdown         = "/afs/desy.de/user/t/tlenz/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_06_JESunc_down/results/analyzer/ntuples/input_weighted/";
+
+TString inputJERcentral      = fileSR;
+TString inputJERup           = "/afs/desy.de/user/t/tlenz/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_06_JERunc_up/results/analyzer/ntuples/input_weighted/";
+TString inputJERdown         = "/afs/desy.de/user/t/tlenz/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_06_JERunc_down/results/analyzer/ntuples/input_weighted/";
+
+TString inputPUcentral      = fileSR;
+TString inputPUup           = "/afs/desy.de/user/t/tlenz/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_06_PUunc_up/results/analyzer/ntuples/input_weighted/";
+TString inputPUdown         = "/afs/desy.de/user/t/tlenz/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_06_PUunc_down/results/analyzer/ntuples/input_weighted/";
+
+TString inputEcaloUnc       = "/afs/desy.de/user/t/tlenz/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_05_METGt0_JetPtGt0_FakeCS/results/analyzer/ntuples/input_weighted/";
+
+TString inputNmissUnc       = "/afs/desy.de/user/t/tlenz/xxl-af-cms/ANALYSIS/workdir/analysis_2015_08_04_METGt0_JetPt0_NmissUnc/results/analyzer/ntuples/input_weighted/";
 
 #endif
