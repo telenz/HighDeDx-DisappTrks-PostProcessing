9272ac70177b85902701c2c0d187d9f9b9a937f1
Teresa Lenz <teresa.lenz@desy.de>
2015-08-21 14:36:13 +0200

    add different limit plots

--- limits/CMSSW_5_3_20/src/HiggsAnalysis-CombinedLimit/runAllLimitsWithCorrectCuts.sh
+++ limits/CMSSW_5_3_20/src/HiggsAnalysis-CombinedLimit/runAllLimitsWithCorrectCuts.sh
@@ -8,6 +8,7 @@ decaywidthTable=("1" "5" "10" "50" "100")   #please specifiy in cm
 filenameIn="datacardsSelectedUnblinded"
 mkdir logFiles/${filenameIn}
 mkdir limits/${filenameIn}
+mkdir limits/${filenameIn}_xsec
 for ctau in "${decaywidthTable[@]}"
 do
     echo "ctau = " $ctau
@@ -19,7 +20,7 @@ do
 	datacardName=$(find ${filenameIn}/ -name "*mass_${mass}GeV_ctau_${ctau}cm.txt" )
 	
 	#combine -M Asymptotic --run expected -C 0.95 -t -1 --minimizerStrategy 2 ${datacardName} > logFiles/${filenameIn}/res_mass_${mass}GeV_ctau_${ctau}cm.log
-	combine -M Asymptotic -C 0.95   --minimizerStrategy 2  ${datacardName} > logFiles/${filenameIn}/res_mass_${mass}GeV_ctau_${ctau}cm.log
+	#combine -M Asymptotic -C 0.95   --minimizerStrategy 2  ${datacardName} > logFiles/${filenameIn}/res_mass_${mass}GeV_ctau_${ctau}cm.log
 	cat logFiles/${filenameIn}/res_mass_${mass}GeV_ctau_${ctau}cm.log
 
 	echo ""
@@ -28,6 +29,8 @@ do
     done
     root -l -b -q plot_limit_WithCorrectCuts.cc+"(\"${filenameIn}\", ${ctau})"
     mv test_ctau${ctau}cm.pdf limits/${filenameIn}/LimitPlot_ctau${ctau}cm.pdf
+    root -l -b -q plot_limit_xsec.cc+"(\"${filenameIn}\", ${ctau})"
+    mv test_ctau${ctau}cm.pdf limits/${filenameIn}_xsec/LimitPlot_ctau${ctau}cm.pdf
 done
 
 rm *.root
